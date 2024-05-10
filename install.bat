87d02525ebb1c9518af6922fd594e5e447908b9d
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-05-10 20:49:05 +0100

    use -r for requirements

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 set REPO_DIR=%~dp0
 set WORK_DIR=%TEMP%\build-pygetm-%RANDOM%
 
-pip install -v -f requirements.txt
+pip install -v -r requirements.txt
 
 cmake -S %REPO_DIR%\extern\pygsw -B %WORK_DIR% -DCMAKE_BUILD_TYPE=Release
 cmake --build %WORK_DIR% --target install --config Release --parallel 4
