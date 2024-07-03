450bbc3a96dac581f87e2f3387df3f7b9846f13e
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-03 17:00:30 +0100

    fix windows install of pygsw and otps2

--- install.bat
+++ install.bat
@@ -1,13 +1,26 @@
 set REPO_DIR=%~dp0
 set WORK_DIR=%TEMP%\build-pygetm-%RANDOM%
 
+@SET PYFABM_DIR=
+FOR /F %%I IN ('python -c "import pyfabm,os;print(os.path.dirname(pyfabm.__file__))"') DO @SET "PYFABM_DIR=%%I"
+
 pip install -v -r requirements.txt
+if errorlevel 1 exit /b 1
 
 cmake -S %REPO_DIR%\extern\pygsw -B %WORK_DIR% -DCMAKE_BUILD_TYPE=Release
-cmake --build %WORK_DIR% --target install --config Release --parallel 4
+if errorlevel 1 exit /b 1
+cmake --build %WORK_DIR% --target pygsw_wheel --config Release --parallel 4
+if errorlevel 1 exit /b 1
+xcopy /E /I "%WORK_DIR%\pygsw" "%PYFABM_DIR%\..\pygetm\pygsw"
+if errorlevel 1 exit /b 1
 
 rmdir /S /Q %WORK_DIR%
+if errorlevel 1 exit /b 1
 cmake -S %REPO_DIR%\extern\python-otps2 -B %WORK_DIR% -DCMAKE_BUILD_TYPE=Release
-cmake --build %WORK_DIR% --target install --config Release --parallel 4
+if errorlevel 1 exit /b 1
+cmake --build %WORK_DIR% --target otps2_wheel --config Release --parallel 4
+if errorlevel 1 exit /b 1
+xcopy /E /I "%WORK_DIR%\otps2" "%PYFABM_DIR%\..\pygetm\otps2"
+if errorlevel 1 exit /b 1
 
 rmdir /S /Q %WORK_DIR%
