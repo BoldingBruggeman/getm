ef4b3dd7dbb4b97f5a316ff6512ec2831620890d
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-07-15 12:32:22 +0100

    get awex and pygotm via conda

--- install.bat
+++ install.bat
@@ -4,23 +4,23 @@ set WORK_DIR=%TEMP%\build-pygetm-%RANDOM%
 @SET PYFABM_DIR=
 FOR /F %%I IN ('python -c "import pyfabm,os;print(os.path.dirname(pyfabm.__file__))"') DO @SET "PYFABM_DIR=%%I"
 
-pip install -v -r requirements.txt
+pip install -v "%REPO_DIR%\python"
 if errorlevel 1 exit /b 1
 
-cmake -S %REPO_DIR%\extern\pygsw -B %WORK_DIR% -DCMAKE_BUILD_TYPE=Release
+cmake -S "%REPO_DIR%\extern\pygsw" -B "%WORK_DIR%" -DCMAKE_BUILD_TYPE=Release
 if errorlevel 1 exit /b 1
-cmake --build %WORK_DIR% --target pygsw_wheel --config Release --parallel 4
+cmake --build "%WORK_DIR%" --target pygsw_wheel --config Release --parallel 4
 if errorlevel 1 exit /b 1
 xcopy /E /I "%WORK_DIR%\pygsw" "%PYFABM_DIR%\..\pygetm\pygsw"
 if errorlevel 1 exit /b 1
 
-rmdir /S /Q %WORK_DIR%
+rmdir /S /Q "%WORK_DIR%"
 if errorlevel 1 exit /b 1
-cmake -S %REPO_DIR%\extern\python-otps2 -B %WORK_DIR% -DCMAKE_BUILD_TYPE=Release
+cmake -S "%REPO_DIR%\extern\python-otps2" -B "%WORK_DIR%" -DCMAKE_BUILD_TYPE=Release
 if errorlevel 1 exit /b 1
-cmake --build %WORK_DIR% --target otps2_wheel --config Release --parallel 4
+cmake --build "%WORK_DIR%" --target otps2_wheel --config Release --parallel 4
 if errorlevel 1 exit /b 1
 xcopy /E /I "%WORK_DIR%\otps2" "%PYFABM_DIR%\..\pygetm\otps2"
 if errorlevel 1 exit /b 1
 
-rmdir /S /Q %WORK_DIR%
+rmdir /S /Q "%WORK_DIR%"
