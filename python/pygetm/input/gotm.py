26451eac20305c456ec1f83a62e75e2968a0798a
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-09-09 14:22:50 +0100

    input.gotm: add get_profiles, allow get_timeseries to retrieve subset of columns

--- python/pygetm/input/gotm.py
+++ python/pygetm/input/gotm.py
@@ -1,6 +1,6 @@
 import argparse
 import datetime
-from typing import Iterable, Mapping
+from typing import Iterable, Mapping, Union
 import os
 
 import cftime
@@ -22,12 +22,16 @@ METEO_UNITS = dict(u10="m s-1", v10="m s-1", tcc="1")
 
 def get_timeseries(
     path: str,
-    columns: Iterable[str],
+    columns: Union[Mapping[int, str], Iterable[str]],
     units: Mapping[str, str] = {},
     long_names: Mapping[str, str] = {},
 ) -> xr.Dataset:
     dts = []
     all_values = []
+    ncol = None
+    if not isinstance(columns, Mapping):
+        columns = dict(enumerate(columns))
+        ncol = max(columns) + 1
     with open(path) as f:
         last_dt = None
         for line in f:
@@ -36,7 +40,7 @@ def get_timeseries(
             dt = datetime.datetime.strptime(line[:19], "%Y-%m-%d %H:%M:%S")
             assert last_dt is None or last_dt <= dt
             items = line[20:].rstrip("\n").split()
-            assert len(items) == len(columns)
+            assert ncol is None or len(items) == ncol
             dts.append(
                 cftime.datetime(
                     dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
@@ -44,9 +48,11 @@ def get_timeseries(
             )
             all_values.append(list(map(float, items)))
 
+    all_values = np.asarray(all_values)
     timevar = xr.DataArray(np.array(dts), dims=("time",))
     name2var = dict(time=timevar)
-    for name, values in zip(columns, np.transpose(all_values)):
+    for icol, name in columns.items():
+        values = all_values[:, icol]
         attrs = {}
         if name in units:
             attrs["units"] = units[name]
@@ -54,6 +60,78 @@ def get_timeseries(
             attrs["long_name"] = long_names[name]
         ar = xr.DataArray(
             values, coords={"time": timevar}, dims=("time",), name=name, attrs=attrs
+        )
+        ar.encoding["source"] = os.path.normpath(path)
+        name2var[name] = ar
+    return xr.Dataset(name2var)
+
+
+def get_profiles(
+    path: str,
+    columns: Union[Mapping[int, str], Iterable[str]],
+    units: Mapping[str, str] = {},
+    long_names: Mapping[str, str] = {},
+) -> xr.Dataset:
+    dts = []
+    all_values = []
+    z = None
+    ncol = None
+    if not isinstance(columns, Mapping):
+        columns = dict(enumerate(columns))
+        ncol = max(columns) + 1
+    with open(path) as f:
+        last_dt = None
+        while 1:
+            line = f.readline()
+            if line.startswith("#"):
+                continue
+            if not line:
+                break
+            dt = datetime.datetime.strptime(line[:19], "%Y-%m-%d %H:%M:%S")
+            assert last_dt is None or last_dt <= dt
+            items = line[20:].rstrip("\n").split()
+            n = int(items[0])
+            up = int(items[1]) == 1
+            values = []
+            current_z = []
+            for i in range(n):
+                line = f.readline()
+                if line.startswith("#"):
+                    continue
+                items = list(map(float, line.rstrip("\n").split()))
+                current_z.append(items.pop(0))
+                assert ncol is None or len(items) == ncol
+                values.append(items)
+            current_z = np.asarray(current_z)
+            if z is None:
+                z = current_z
+            else:
+                assert (current_z == z).all()
+            dts.append(
+                cftime.datetime(
+                    dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
+                )
+            )
+            all_values.append(values)
+
+    all_values = np.asarray(all_values)
+
+    timevar = xr.DataArray(np.array(dts), dims=("time",))
+    zvar = xr.DataArray(z, dims=("z",), attrs={"positive": "up", "units": "m"})
+    name2var = dict(time=timevar)
+    for icol, name in columns.items():
+        values = all_values[..., icol]
+        attrs = {}
+        if name in units:
+            attrs["units"] = units[name]
+        if name in long_names:
+            attrs["long_name"] = long_names[name]
+        ar = xr.DataArray(
+            values,
+            coords={"time": timevar, "z": zvar},
+            dims=("time", "z"),
+            name=name,
+            attrs=attrs,
         )
         ar.encoding["source"] = os.path.normpath(path)
         name2var[name] = ar
