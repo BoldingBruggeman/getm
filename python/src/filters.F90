b986caad7af258a5cd8c7c45df6472c64e6e1902
Karsten Bolding <karsten@bolding-bruggeman.com>
2024-10-25 13:07:36 +0200

    added documentation to filters.F90

--- python/src/filters.F90
+++ python/src/filters.F90
@@ -6,7 +6,12 @@ module c_filters
 
 contains
 
-   subroutine c_horizontal_filter(imin, imax, jmin, jmax, kmax, halox, haloy, mask, w, var) bind(c)
+   subroutine c_horizontal_filter(imin, imax, jmin, jmax, kmax, &
+                                  halox, haloy, mask, w, var) bind(c)
+
+     !! Simple horizontal filter where the central value is weighted by
+     !! w and the avarage of the - active - neighbors by 1-w.
+
       integer(c_int), intent(in), value :: imin, imax, jmin, jmax, kmax
       integer(c_int), intent(in), value :: halox
       integer(c_int), intent(in), value :: haloy
@@ -16,24 +21,25 @@ contains
       real(c_double), intent(inout) :: var(_A_,kmax)
 #undef _A_
 
-      integer :: i, j, k
-      integer :: rc
       real(c_double), allocatable :: x(:,:,:)
       integer :: n1, n2, n3, n4, n5
+      integer :: rc
+      integer :: i, j, k
 
-      if (w < 0. .or. w > 0.25) return
+      if (w < 0._c_double .or. w > 0.25_c_double) return
 
       allocate(x, source=var, stat=rc)
       if (rc /= 0) stop 'c_horizontal_filter: Error allocating x'
       do j = jmin, jmax
          do i = imin, imax
-            if (mask(i,j) == 0) cycle
+            if (mask(i,j) < 1) cycle
+
             n1=min(1,mask(i-1,j))
             n2=min(1,mask(i+1,j))
             n3=min(1,mask(j-1,j))
             n4=min(1,mask(j+1,j))
             n5=n1+n2+n3+n4
-            var(i,j,:) = w*var(i,j,:)+(1.-w) &
+            var(i,j,:) = w*var(i,j,:)+(1._c_double-w) &
                         *( n1*x(i-1,j,:) &
                           +n2*x(i+1,j,:) &
                           +n3*x(i,j-1,:) &
@@ -42,7 +48,13 @@ contains
       end do
    end subroutine
 
-   subroutine c_vertical_filter(nfilter, imin, imax, jmin, jmax, kmax, halox, haloy, mask, w, var) bind(c)
+   subroutine c_vertical_filter(nfilter, imin, imax, jmin, jmax, kmax, &
+                                halox, haloy, mask, w, var) bind(c)
+
+     !! Simple vartical filter where the central value is weighted by
+     !! w and the two neighbors by 1-w/2.
+     !! The filter will be applied nfilter times.
+
       integer(c_int), intent(in), value :: nfilter
       integer(c_int), intent(in), value :: imin, imax, jmin, jmax, kmax
       integer(c_int), intent(in), value :: halox
@@ -53,24 +65,23 @@ contains
       real(c_double), intent(inout) :: var(_A_, kmax)
 #undef _A_
 
+      real(c_double) :: col(0:kmax)
+      real(c_double) :: w1
       integer :: i, j, k, n
 
-      real(c_double) :: col(0:kmax)
-      real(c_double) :: w1, w2
+      if (nfilter < 1 .or. (w < 0._c_double .or. w > 1._c_double)) return
 
-      if (nfilter < 1 .or. (w < 0. .or. w > 1.)) return
-
-      w1 = w
-      w2 = (1-w)/2
+      w1 = (1_c_double-w)/2
       do n = 1, nfilter
          do j = jmin, jmax
             do i = imin, imax
-               if (mask(i,j) == 0) cycle
+               if (mask(i,j) < 1) cycle
+
                col(kmax) = var(i,j,kmax)
                col(1) = var(i,j,1)
                col(0) = col(1)
                do k = 2, kmax-1
-                  col(k) = w1*col(k)+w2*(var(i,j,k-1)+var(i,j,k+1))
+                  col(k) = w*col(k)+w1*(var(i,j,k-1)+var(i,j,k+1))
                end do
                var(i,j,:) = col(1:kmax)
             end do
